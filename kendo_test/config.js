87c6a88c5bd821656aeff4bad501ff3aa4b84e9c
Артемий Данилов <quiet.temkin@gmail.com>
2014-09-03 06:51:28 -0500

    Changes made by Артемий Данилов (quiet.temkin@gmail.com).

---
+++ kendo_test/config.js
@@ -0,0 +1,1 @@
+var config = {host: 'http://mint.cheby.ru'};
--- kendo_test/scripts/app.js
+++ kendo_test/scripts/app.js
@@ -1,4 +1,3 @@
-
 (function () {
 
     // store a reference to the application object that will be created
@@ -6,28 +5,8 @@
     var app;
 
     // create an object to store the models for each view
-    window.APP = {
-      models: {
-        home: {
-          title: 'Home'
-        },
-        settings: {
-          title: 'Settings'
-        },
-        contacts: {
-          title: 'Contacts',
-          ds: new kendo.data.DataSource({
-            data: [{ id: 1, name: 'Bob' }, { id: 2, name: 'Mary' }, { id: 3, name: 'John' }]
-          }),
-          alert: function(e) {
-            alert(e.data.name);
-          }
-        }
-      }
-    };
-
     // this function is called by Cordova when the application is loaded by the device
-    document.addEventListener('deviceready', function () {  
+    document.addEventListener('deviceready', function () {
       
       // hide the splash screen as soon as the app is ready. otherwise
       // Cordova will wait 5 very long seconds to do it for you.
@@ -43,10 +22,25 @@
         skin: 'flat',
 
         // the application needs to know which view to load first
-        initial: 'views/home.html'
+        initial: config.id_session ? 'view/users_activity_chart.html' : 'login.html'
       });
+
 
     }, false);
 
 
 }());
+
+function loadDataSource(type) {
+    
+    return new kendo.data.DataSource(
+        {
+            transport: {
+                read: {
+                  url: config.host + "/?type=" + type + "&id=1&sid=" + config.id_session,
+                  dataType: "json"
+                }
+              }
+        }
+	)
+};
