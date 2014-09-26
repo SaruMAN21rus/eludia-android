744321f5da7fa38aa4030451c7d25744834483dd
Alexey Mukin <saruman21rus@gmail.com>
2014-09-26 08:25:14 -0500

    Changes made by Alexey Mukin (saruman21rus@gmail.com).

--- eludia/scripts/app.js
+++ eludia/scripts/app.js
@@ -1,3 +1,5 @@
+var CLICK_TAP = false;
+
 (function () {
 
     // store a reference to the application object that will be created
@@ -63,13 +65,15 @@ function swipe_docs_menu (e) {
 }
 
  function swipe_users_activity (e) {
-	if (e.direction === 'right' && window.location.hash === "#view/reports/users_activity_chart.html") {
+    if (e.direction === 'right' && window.location.hash === "#view/reports/users_activity_chart.html") {
     	window.location.hash = "#view/reports_menu.html"
 	} else {
         window.location.hash = "#view/reports/users_activity_chart.html"
+        CLICK_TAP = true;
 	}
 	if (e.direction === 'left') {
         window.location.hash = "#view/reports/users_activity_data.html"
+        CLICK_TAP = true;
     }
  };
 
@@ -78,26 +82,32 @@ function swipe_docs_menu (e) {
     	window.location.hash = "#view/reports_menu.html"
 	} else {
         window.location.hash = "#view/reports/clients_report_pie_chart.html"
+        CLICK_TAP = true;
 	}         
 	if (e.direction === 'left') {
         window.location.hash = "#view/reports/clients_report_pie_data.html"
+        CLICK_TAP = true;
     }
  };
 
- function swipe_clients_report (e) {
+function swipe_clients_report (e) {
 	if (e.direction === 'right' && window.location.hash === "#view/reports/clients_report_chart.html") {
     	window.location.hash = "#view/reports_menu.html"
 	} else {
         window.location.hash = "#view/reports/clients_report_chart.html"
+        CLICK_TAP = true;
 	}         
 	if (e.direction === 'left') {
         window.location.hash = "#view/reports/clients_report_data.html"
+        CLICK_TAP = true;
     }
  };
 
 function reloadBotMenu(title, descriprion) {
     $("#bottom-menu #fade-out-right-pnl #page-title #text").text(title);
     $("#title #fade-in-up-pnl .title").html(title + "<br><span class=\"description\">"+ descriprion + "</span>");
+    $('#btn-show-menu').css("background-image", "url(styles/images/btn_show_bg.png)");
+    showMenu=false;
     $("#bottom-menu").show();
 }; 
 
@@ -105,5 +115,20 @@ function beforeHide() {
     $('.view-title').width('100%');
     $('.view-title').height('100%');
     $("#bottom-menu").hide();
-}   
-  
+}
+
+function onFooterTapSelect(e) {
+    CLICK_TAP = true;
+}
+
+function beforeHidePageWithTap() {
+    if (CLICK_TAP) {
+        $('.view-title').width('100%');
+        $('.view-title').height('100%');
+        $('#btn-show-menu').css("background-image", "url(styles/images/btn_show_bg.png)");
+        showMenu=false;
+        CLICK_TAP = false;
+    } else {
+        beforeHide();
+    }
+}
